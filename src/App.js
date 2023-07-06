9da035a4b5b03642251f388b01d860f2a35be0dc
Türkalp Kucur <turkalp.kucur@gmail.com>
2023-07-06 11:33:49 +0300

    fazlalıklar atıldı (2)
    fazlalıklar atıldı (2)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+
 import { Calendar } from './pages';
 import './App.css';
 
@@ -41,15 +41,7 @@ const App = () => {
 
             </TooltipComponent>
           </div>
-          {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-              <Sidebar />
-            </div>
-          ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">
-              <Sidebar />
-            </div>
-          )}
+
           <div
             className={
               activeMenu
@@ -58,7 +50,7 @@ const App = () => {
             }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-              <Navbar />
+
             </div>
             <div>
               {themeSettings && (<ThemeSettings />)}
@@ -72,7 +64,7 @@ const App = () => {
                 {/* charts  */}
               </Routes>
             </div>
-            <Footer />
+
           </div>
         </div>
       </BrowserRouter>
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -3,7 +3,7 @@ import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, 
 import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
 
 import { scheduleData } from '../data/dummy';
-import { Header } from '../components';
+
 
 // eslint-disable-next-line react/destructuring-assignment
 const PropertyPane = (props) => <div className="mt-5">{props.children}</div>;
@@ -23,7 +23,7 @@ const Scheduler = () => {
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="App" title="Calendar" />
+
       <ScheduleComponent
         height="650px"
         ref={(schedule) => setScheduleObj(schedule)}
@@ -32,7 +32,7 @@ const Scheduler = () => {
         dragStart={onDragStart}
       >
         <ViewsDirective>
-          { ['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => <ViewDirective key={item} option={item} />)}
+          {['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => <ViewDirective key={item} option={item} />)}
         </ViewsDirective>
         <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
       </ScheduleComponent>
